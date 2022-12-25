421f4c028d9f8bd1478c926940fef412c4facdb6
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2022-12-25 17:38:50 -0300

    correção da posição da imagem no componente Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 
 export default function Header() {
   return(
-    <div className="h-50vh sm:h-86vh w-full relative flex justify-center bg-anil">
+    <div className="h-70vh sm:h-86vh w-full relative flex justify-center bg-anil">
       <div className="w-full h-full flex items-end justify-center sm:justify-center absolute">
         <img
           src={require('../imagens/Pokémon_logo.png')}
-          className="w-60vw sm:w-30vw transition-all pb-5vh sm:pb-7vh" 
+          className="w-80vw sm:w-30vw transition-all pb-5vh sm:pb-7vh" 
           alt=""
         />
       </div>
       <img
         src={require('../imagens/wallpaper/YoutTeam.jpg')}
         alt=""
-        className="py-10vh sm:py-0 h-full sm:h-4/5 object-cover object-start mb-10"
+        className="py-10vh sm:py-0 h-full sm:h-4/5 object-cover object-left sm:object-center mb-10"
       />
     </div>
   );
