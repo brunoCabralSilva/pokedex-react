f1e521a361edf5e55d0061b92aae3923a43ecdab
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2022-12-29 14:03:45 -0300

    Conclusão da versão básica de detailsMove

--- src/pages/DetailsMove.jsx
+++ src/pages/DetailsMove.jsx
@@ -1,12 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import Footer from '../components/Footer';
+import { useHistory } from 'react-router-dom';
 import Nav from '../components/Nav';
 import { useParams } from 'react-router-dom';
 import { getMove } from '../fetchs';
+import Pokemon from '../components/Pokemon';
+import contexto from '../context';
+import AllDataTypes from '../components/AllDataTypes';
 
 export default function DetailsMove() {
   const [move, setMove] = useState({});
   const params = useParams();
+  const history = useHistory();
+  const context = useContext(contexto);
+  const { numberPokemon, letraMaiuscula } = context;
   useEffect(() => {
     const getMoveByName = async () => {
       const get = await getMove((params.name));
@@ -16,32 +23,57 @@ export default function DetailsMove() {
   },[]);
 
   return (
-    <div className="w-full flex flex-col items-center min-h-screen">
+    <div className="w-full flex flex-col items-center min-h-screen bg-gradient-to-l via-white from-anil to-white">
       <Nav />
-      <div className="flex flex-row items-left justify-center">
+      <div
+        className="z-40 text-black text-4xl p-2 fixed top-0 left-0 w-full flex justify-left bg-gradient-to-l via-white from-anil to-white"
+        onClick={ () => { 
+          history.push('/moves');
+        } }
+      >
+        <img
+          src={ require('../imagens/arrow-left.png') }
+          alt="seta"
+          className="w-10"
+        />
+      </div>
+      <div className="flex flex-row items-center justify-center lg:pl-7">
         {
           Object.values(move).length > 0 &&
-          <div>
-            <p>{ move.name }</p>
-            <p>id { move.id }</p>
-            <p>Type: {move.type.name}</p>
-            <p>Accuracy { move.accuracy }</p>
-            <p>damage class { move.damage_class.name }</p>
-            <p>Efeito: { move.flavor_text_entries.find((text) => text.language.name === 'es').flavor_text }</p>
-            <p>Efeito: { move.effect_entries[0].effect }</p> /fazer um map
-            <p>Power: {move.power}</p>
-            <p>pp: {move.pp}</p>
-            <p>Priority: {move.priority}</p>
-            <p>Target: {move.target.name}</p>
-            <div>
-              <p>{`Learned by ${move.learned_by_pokemon.length} pokemon`}</p>
-              <div className="grid grid-cols-4">
+          <div className="flex flex-col items-start justify-center px-7">
+            <div className="flex flex-col w-full justify-start pt-10 gap-3">
+              { AllDataTypes(move.type.name).image }
+              <p className="text-3xl sm:text-2xl md:text-4xl">{ letraMaiuscula(move.name) }</p>
+              <p className="pb-2 text-1xl text-gray-700"># { move.id }</p>
+            </div>
+            <div className="pt-5">
+            <p className="text-3xl sm:text-2xl md:text-4xl">Dados</p>
+              <p className="pt-5">Precisão: { move.accuracy }</p>
+              <p className="pt-1">Classe de dano: { letraMaiuscula(move.damage_class.name) }</p>
+              <p className="pt-1">Poder: {move.power}</p>
+              <p className="pt-1">PP: {move.pp}</p>
+              <p className="pt-1">Prioridade: {move.priority}</p>
+              <p className="pt-1">Alvo: {move.target.name}</p>
+              <p className="pt-10 text-3xl sm:text-2xl md:text-4xl">Efeito</p>
+              <p className="pt-5">
+                Espanhol: { move.flavor_text_entries.find((text) => text.language.name === 'es').flavor_text }
+              </p>
+              <p className="pt-3">
+                Inglês: { move.effect_entries[0].effect }
+              </p> /fazer um map
+            </div>
+            <div className="flex flex-col items-center">
+              <p className="w-full text-3xl sm:text-2xl md:text-4xl py-10">
+                {`Aprendido por ${move.learned_by_pokemon.length} Pokémon:`}
+              </p>
+              <div className="bg-white p-1 gap-3 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4">
                 {
                   move.learned_by_pokemon.map((poke) => (
-                    <div>
-                      <p>{ poke.name }</p>
-                      <p>{ poke.url }</p>
-                    </div>
+                    <Pokemon
+                    name={poke.name}
+                    id = {numberPokemon(poke)}
+                    dataPokemon={poke}
+                    />
                   ))
                 }
               </div>
