d7ec54cf5ca9242fd17a1197d051a015bad03204
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2023-01-20 23:40:37 -0300

    alterando o BrowserRouter para HashRouter

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import PokeProvider from './context/PokeProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <PokeProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </PokeProvider>
   </React.StrictMode>
 );
